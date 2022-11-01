36c2f9ce8e2da4696c565ac5323c6e9db61aaff5
Gilson Chariguaman <gilson258@live.com>
2022-11-01 17:33:24 -0500

    deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -65,83 +65,83 @@ class DatabaseSeeder extends Seeder
         ]);
 
         //FAKE DATA
-        for ($i = 0; $i < 10; $i++) {
-            $category = ProductCategory::create([
-                'name' => $faker->Company,
-                'shortDescription' => $shortDescription,
-                'status' => 1
-            ]);
-        }
+        // for ($i = 0; $i < 10; $i++) {
+        //     $category = ProductCategory::create([
+        //         'name' => $faker->Company,
+        //         'shortDescription' => $shortDescription,
+        //         'status' => 1
+        //     ]);
+        // }
 
 
-        for ($i = 0; $i < 20; $i++) {
-            $product = Product::create([
-                'categoryId' => rand(1, 10),
-                'name' => $faker->Company,
-                'shortDescription' => $shortDescription,
-                'longDescription' => $longDescription,
-                'discountRate' => rand(10, 80),
-                'price' => $faker->numerify('##.##'),
-                'urlImage' => 'images/placeholder.jpg',
-                'unit' => 'c/u',
-                'code' => 'PO'.$i,
-                'status' => 1,
-            ]);
+        // for ($i = 0; $i < 20; $i++) {
+        //     $product = Product::create([
+        //         'categoryId' => rand(1, 10),
+        //         'name' => $faker->Company,
+        //         'shortDescription' => $shortDescription,
+        //         'longDescription' => $longDescription,
+        //         'discountRate' => rand(10, 80),
+        //         'price' => $faker->numerify('##.##'),
+        //         'urlImage' => 'images/placeholder.jpg',
+        //         'unit' => 'c/u',
+        //         'code' => 'PO'.$i,
+        //         'status' => 1,
+        //     ]);
 
-            for ($j = 0; $j < 10; $j++) {
-                $name = $faker->firstNameMale;
-                $lastname = $faker->lastname;
-                $profile = substr($name, 0, 1).substr($lastname, 0, 1);
-                $arrayColor = ['#84b6f4','#f9a59a','#fa5f49','#bc98f3','#f47e8e','#ff9c9c','#f45572','#6a9eda','#84b6f4','#c0a0c3'];
-                $bgProfile = array_rand($arrayColor, 1);
-                $comment = new Comment([
-                    'name' => $name,
-                    'lastname' => $lastname,
-                    'profile' => $profile,
-                    'bgProfile' => $arrayColor[$bgProfile],
-                    'email' => 'client@email.com',
-                    'rating' => rand(0, 5),
-                    'message' => $shortDescription,
-                    'status' => 1
-                ]);
-                $product->comments()->save($comment);
-            }
+        //     for ($j = 0; $j < 10; $j++) {
+        //         $name = $faker->firstNameMale;
+        //         $lastname = $faker->lastname;
+        //         $profile = substr($name, 0, 1).substr($lastname, 0, 1);
+        //         $arrayColor = ['#84b6f4','#f9a59a','#fa5f49','#bc98f3','#f47e8e','#ff9c9c','#f45572','#6a9eda','#84b6f4','#c0a0c3'];
+        //         $bgProfile = array_rand($arrayColor, 1);
+        //         $comment = new Comment([
+        //             'name' => $name,
+        //             'lastname' => $lastname,
+        //             'profile' => $profile,
+        //             'bgProfile' => $arrayColor[$bgProfile],
+        //             'email' => 'client@email.com',
+        //             'rating' => rand(0, 5),
+        //             'message' => $shortDescription,
+        //             'status' => 1
+        //         ]);
+        //         $product->comments()->save($comment);
+        //     }
 
-            for ($k = 0; $k < 2; $k++) {
-                $imageUpload = new Picture([
-                    'urlImage' => 'images/placeholder.jpg',
-                ]);
-                $product->pictures()->save($imageUpload);
-            }
-        }
+        //     for ($k = 0; $k < 2; $k++) {
+        //         $imageUpload = new Picture([
+        //             'urlImage' => 'images/placeholder.jpg',
+        //         ]);
+        //         $product->pictures()->save($imageUpload);
+        //     }
+        // }
 
 
-        for ($i = 0; $i < 20; $i++) {
-            $service = Service::create([
-                'name' => $faker->Company,
-                'shortDescription' => $shortDescription,
-                'longDescription' => $longDescription,
-                'urlImage' => 'images/placeholder.jpg',
-                'status' => 1,
-            ]);
-        }
+        // for ($i = 0; $i < 20; $i++) {
+        //     $service = Service::create([
+        //         'name' => $faker->Company,
+        //         'shortDescription' => $shortDescription,
+        //         'longDescription' => $longDescription,
+        //         'urlImage' => 'images/placeholder.jpg',
+        //         'status' => 1,
+        //     ]);
+        // }
 
-        for ($i = 0; $i < 10; $i++) {
-            $banner = Banner::create([
-                'urlImage' => 'images/placeholder.jpg',
-                'status' => 1,
-            ]);
-        }
+        // for ($i = 0; $i < 10; $i++) {
+        //     $banner = Banner::create([
+        //         'urlImage' => 'images/placeholder.jpg',
+        //         'status' => 1,
+        //     ]);
+        // }
 
-        for ($i = 0; $i < 20; $i++) {
-            $message = \App\Models\Message::create([
-                'name' => $faker->Company,
-                'email' => 'client@email.com',
-                'message' => $shortDescription,
-                'read' => 1,
-                'status' => 1
-            ]);
-        }
+        // for ($i = 0; $i < 20; $i++) {
+        //     $message = \App\Models\Message::create([
+        //         'name' => $faker->Company,
+        //         'email' => 'client@email.com',
+        //         'message' => $shortDescription,
+        //         'read' => 1,
+        //         'status' => 1
+        //     ]);
+        // }
 
     }
 }
